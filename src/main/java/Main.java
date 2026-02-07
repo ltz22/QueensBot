6647a9062f16901425758d0a6a16fe2abc6c4e38
Letong Zhang <L.Zhang-35@student.tudelft.nl>
2026-02-07 18:24:06 +0100

    Adjusted order of execution to improve browser initiation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,11 @@ public class Main {
     public static void main(String[] args) {
         QueensBot bot = null;
         try(Scanner scanner = new Scanner(System.in);) {
-            System.out.println("\nSwitch to the browser window.");
-            System.out.println("Press Enter when ready to start...");
-            scanner.nextLine();
             bot = new QueensBot();
             bot.start();
 
-            //System.out.println("Starting in 3 seconds...");
-            //Thread.sleep(3000);
+            System.out.println("Press Enter to close the browser...");
+            scanner.nextLine();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
